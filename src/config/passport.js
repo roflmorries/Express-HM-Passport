e5f1b580823fc57fc6483b34a681fb6428b59593
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-15 20:34:12 +0300

    integrated Google OAuth authentication

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
-import { findUserByEmail, findUserById, validatePassword } from "../models/user.js";
+import { createUser, findUserByEmail, findUserByGoogleId, findUserById, validatePassword } from "../models/user.js";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
 passport.use(new LocalStrategy(
   {usernameField: 'email'},
@@ -14,6 +15,31 @@ passport.use(new LocalStrategy(
     return done(null, user)
   }
 ));
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
+}, async (accessToken, refreshToken, profile, done) => {
+  let user = findUserByGoogleId(profile.id);
+
+  if (!user && profile.emails && profile.emails.length > 0) {
+    user = findUserByEmail(profile.emails[0].value);
+    if (user && !user.googleId) {
+      user.googleId = profile.id;
+    }
+  }
+
+  if (!user) {
+    user = await createUser({
+      email: profile.emails[0].value,
+      password: null,
+      googleId: profile.id
+    });
+  }
+
+  return done(null, user);
+}));
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -5,13 +5,12 @@ import * as authController from '../controllers/userController.js'
 const router = Router();
 
 router.post('/register', authController.register);
-
 router.post('/login', passport.authenticate('local'), authController.login);
-
 router.post('/logout', authController.logout);
-
 router.post('/forgot', authController.forgotPassword);
-
 router.post('/reset/:token', authController.resetPassword);
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/auth/login' }), authController.login);
 
 export default router;
