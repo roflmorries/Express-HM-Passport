87ce70bc022d453a1ccc90389927187fdca74ada
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-15 16:26:14 +0300

    added passport config

---
+++ src/config/passport.js
@@ -0,0 +1,26 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { findUserByEmail, findUserById, validatePassword } from "../models/user";
+
+passport.use(new LocalStrategy(
+  {usernameField: 'email'},
+  async (email, password, done) => {
+    console.log(`User email: ${email}`);
+    console.log(`User password: ${password}`);
+    const user = findUserByEmail(email);
+    if (!user) return done(null, false, { message: 'User with this email doesnt exists' });
+    const valid = await validatePassword(user, password);
+    if (!valid) return done(null, false, { message: 'Incorrect password' });
+    return done(null, user)
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  const user = findUserById(id);
+  if (user) done(null, user);
+  else done(null, false);
+})
