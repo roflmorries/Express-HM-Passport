738801d88abb21f17527c044c817ef76c7f455b8
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-18 23:51:43 +0300

    added db config

---
+++ src/config/db.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+import 'dotenv/config'
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${DB_PASSWORD}@cluster0.lhqyyek.mongodb.net/`;
+const client = new MongoClient(uri);
+const dbName = process.env.DB_NAME;
+
+export const connectDB = async () => {
+  try {
+    await client.connect();
+    db = client.db(dbName);
+    console.log('Connected');
+    return db;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getDB = () => {
+  if (!db) {
+    throw new Error('Database not connected') 
+  }
+}
