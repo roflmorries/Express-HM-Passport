034562e939d7538936fa12f72c56d2b421823415
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-19 14:34:37 +0300

    updated Local & Google strategy

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,13 +2,14 @@ import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import { createUser, findUserByEmail, findUserByGoogleId, findUserById, validatePassword } from "../models/user.js";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { getDB } from "./db.js";
 
 passport.use(new LocalStrategy(
   {usernameField: 'email'},
   async (email, password, done) => {
     console.log(`User email: ${email}`);
     console.log(`User password: ${password}`);
-    const user = findUserByEmail(email);
+    const user = await findUserByEmail(email);
     if (!user) return done(null, false, { message: 'User with this email doesnt exists' });
     const valid = await validatePassword(user, password);
     if (!valid) return done(null, false, { message: 'Incorrect password' });
@@ -21,11 +22,15 @@ passport.use(new GoogleStrategy({
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, async (accessToken, refreshToken, profile, done) => {
-  let user = findUserByGoogleId(profile.id);
+  let user = await findUserByGoogleId(profile.id);
 
   if (!user && profile.emails && profile.emails.length > 0) {
-    user = findUserByEmail(profile.emails[0].value);
+    user = await findUserByEmail(profile.emails[0].value);
     if (user && !user.googleId) {
+      const db = await getDB();
+      await db.collection('users').updateOne(
+        { _id: user._id }, { $set: { googleId: profile.id } }
+      )
       user.googleId = profile.id;
     }
   }
@@ -42,11 +47,11 @@ passport.use(new GoogleStrategy({
 }));
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, user._id.toString());
 });
 
-passport.deserializeUser((id, done) => {
-  const user = findUserById(id);
+passport.deserializeUser(async (id, done) => {
+  const user = await findUserById(id);
   if (user) done(null, user);
   else done(null, false);
 })
